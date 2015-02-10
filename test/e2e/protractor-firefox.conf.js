780aee2aa2888c2bc36b607173501a7de01bf222
Mark Chapman <mark.chapman@gmail.com>
2015-02-10 17:37:26 +0000

    feat(tests) Takes screenshots of tests so regressions are easy to spot

--- test/e2e/protractor-firefox.conf.js
+++ test/e2e/protractor-firefox.conf.js
@@ -1,3 +1,8 @@
+'use strict';
+
+var ScreenShotReporter = require('protractor-screenshot-reporter');
+var path = require('path');
+
 exports.config = {
   specs: [
     './**/*.spec.js'
@@ -6,5 +11,26 @@ exports.config = {
     browserName: 'firefox'
   },
   directConnect: true,
-  baseUrl: 'http://localhost:9000'
+  baseUrl: 'http://localhost:9000',
+
+  onPrepare: function() {
+
+    // Disable animations so e2e tests run more quickly
+    var disableNgAnimate = function() {
+      angular.module('disableNgAnimate', []).run(['$animate', function($animate) {
+        $animate.enabled(false);
+      }]);
+    };
+
+    browser.addMockModule('disableNgAnimate', disableNgAnimate);
+
+    // Add a screenshot reporter and store screenshots to `/tmp/screnshots`:
+    jasmine.getEnv().addReporter(new ScreenShotReporter({
+      baseDirectory: 'test/screen_tests/screenshots',
+      pathBuilder: function (spec, descriptions, results, capabilities) {
+        return path.join(capabilities.caps_.browserName + '-' + descriptions.reverse().join('-'));
+      }
+    }));
+  }
+
 };
