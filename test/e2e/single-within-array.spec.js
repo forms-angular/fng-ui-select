78a49313268e6ad2d2861b37ae8fcdfc07c61110
Mark Chapman <mark.chapman@gmail.com>
2017-02-17 15:17:50 +0000

    chore(dep) Keep in step with forms-angular version numbers

--- test/e2e/single-within-array.spec.js
+++ test/e2e/single-within-array.spec.js
@@ -67,13 +67,13 @@ describe('Single control in array', function () {
         field = $('#cg_f_nested_singleAjax input.select2-input');
         field.clear();
         field.sendKeys('Is');
-
+        element.all(by.css('#ui-select-choices-row-2-0')).first().click();
         expect(element(by.css('#cg_f_nested_singleAjax .select2-choice.ui-select-match')).getText()).toMatch('IsAccepted John true 89');
         element(by.css('#saveButton')).click();
         expect(element(by.css('#cg_f_nested_singleAjax .select2-choice.ui-select-match')).getText()).toMatch('IsAccepted John true 89');
     });
 
-    xit('should handle filtered Ajax lookup', function () {
+    it('should handle filtered Ajax lookup', function () {
         browser.get('/#/test_fng_ui_select/new');
         var match = $('#cg_f_filteredAjax a.select2-choice.ui-select-match');
         expect(match.getText()).toEqual('Start typing...');
@@ -84,6 +84,9 @@ describe('Single control in array', function () {
 
         expect(element.all(by.css('.select2-result-label.ui-select-choices-row-inner')).count()).toEqual(1);
         element.all(by.css('.select2-result-label.ui-select-choices-row-inner')).get(0).click();
+        expect(match.getText()).toMatch('Jones Alan true 93');
+        element(by.css('#saveButton')).click();
+        match = $('#cg_f_filteredAjax a.select2-choice.ui-select-match');
         expect(match.getText()).toMatch('Jones Alan true 93');
     });
 
