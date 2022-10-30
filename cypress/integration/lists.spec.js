4fae0558ded66358c385aeca454e8129e1c109cd
Mark Chapman <mark.chapman@gmail.com>
2022-10-30 22:44:19 +0000

    Fix 2e2 tests

--- cypress/integration/lists.spec.js
+++ cypress/integration/lists.spec.js
@@ -8,14 +8,14 @@ describe('Lists', function () {
     });
     it('Creates a new record with multiple shelves', function () {
         cy.visit('/#/k_referencing_self_collection/new');
-        cy.get('#f_warehouse_name').type("m" + unique + " Warehouse");
+        cy.get('#f_warehouse_name').type("m".concat(unique, " Warehouse"));
         cy.get('#add_f_shelves_btn').click().click().click();
-        cy.get('#f_shelvesList_0 > .row-fluid > #cg_f_shelves_location > .controls > .ng-pristine').type('Top');
-        cy.get('#f_shelvesList_1 > .row-fluid > #cg_f_shelves_location > .controls > .ng-pristine').type('Middle');
-        cy.get('#f_shelvesList_2 > .row-fluid > #cg_f_shelves_location > .controls > .ng-pristine').type('Bottom');
+        cy.get('#f_shelves_location_0').type('Top');
+        cy.get('#f_shelves_location_1').type('Middle');
+        cy.get('#f_shelves_location_2').type('Bottom');
         cy.get('#add_f_items_btn').click();
-        cy.get('#f_itemsList_0 > .row-fluid > #cg_f_items_description > .controls > .ng-pristine').type('Item 1');
-        cy.get('#f_itemsList_0 > .row-fluid > #cg_f_items_shelf > .controls > .ng-pristine').select('Middle');
+        cy.get('#f_items_description_0').type('Item 1');
+        cy.get('#f_items_shelf_0').select('Middle');
         cy.get('#add_f_cleanedShelves').click().click();
         cy.get('#f_cleanedShelves_0').select('Middle');
         cy.get('#f_cleanedShelves_1').select('Top');
@@ -24,12 +24,12 @@ describe('Lists', function () {
     });
     it('Creates a new record with a single shelf', function () {
         cy.visit('/#/k_referencing_self_collection/new');
-        cy.get('#f_warehouse_name').type("s" + unique + " Warehouse");
+        cy.get('#f_warehouse_name').type("s".concat(unique, " Warehouse"));
         cy.get('#add_f_shelves_btn').click();
-        cy.get('#f_shelvesList_0 > .row-fluid > #cg_f_shelves_location > .controls > .ng-pristine').type('Only');
+        cy.get('#f_shelves_location_0').type('Only');
         cy.get('#add_f_items_btn').click();
-        cy.get('#f_itemsList_0 > .row-fluid > #cg_f_items_description > .controls > .ng-pristine').type('Item 1');
-        cy.get('#f_itemsList_0 > .row-fluid > #cg_f_items_shelf > .controls > .ng-pristine').select('Only');
+        cy.get('#f_items_description_0').type('Item 1');
+        cy.get('#f_items_shelf_0').select('Only');
         cy.get('#add_f_cleanedShelves').click();
         cy.get('#f_cleanedShelves_0').select('Only');
         cy.get('#f_favouriteShelf').select('Only');
@@ -39,7 +39,7 @@ describe('Lists', function () {
         cy.visit('/#/l_referencing_list_in_other_document/new');
         cy.get('#f_description').type('An item in multi');
         cy.get('.select2-arrow > b').click();
-        cy.get('.ui-select-search').type("m" + unique);
+        cy.get('.ui-select-search').type("m".concat(unique));
         cy.get('.select2-result-label > .ng-binding').click();
         cy.get('#f_shelf').select('Top');
     });
@@ -47,7 +47,7 @@ describe('Lists', function () {
         cy.visit('/#/l_referencing_list_in_other_document/new');
         cy.get('#f_description').type('An item in single');
         cy.get('.select2-arrow > b').click();
-        cy.get('.ui-select-search').type("s" + unique);
+        cy.get('.ui-select-search').type("s".concat(unique));
         cy.get('.select2-result-label > .ng-binding').click();
         cy.get('#f_shelf').select('Only');
     });
